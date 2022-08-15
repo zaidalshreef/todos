804e182221dbf27c9c3ef117760058d7144c6505
zaidalsharif <zaidalshreef@gmail.com>
2022-08-15 17:29:03 +0300

    todosInReactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  function  addTask(event) {
+    event.preventDefault();
+    if(task !== "") {
+    setTodos((todos) => {
+      setTask("");
+    return [...todos, task]
+  });
+}
+  }
+
+  function deleteTask(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">best todo app ever</header>
+      <form >
+      <input
+        type="text"
+        placeholder="Enter your task"
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <button type="submit" onClick={addTask}>
+        Submit
+      </button>
+      </form>
+      
+      <ul>
+        {todos.map((todo,index) => (
+          <li onClick={() => deleteTask(index)}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
